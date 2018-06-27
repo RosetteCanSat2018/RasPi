af44f6d141c84a838811ec81062a5a0ef46547bb
Tsuyoshi Nakashima <nakashima@lss.mes.titech.ac.jp>
2018-06-27 15:23:21 +0900

    modify a lot of misses

--- GPS_test.cpp
+++ GPS_test.cpp
@@ -9,17 +9,12 @@ Sensor sensor;
 
 int main()
 {
-	char GPSData[2]
+	float GPSData[2];
 	sensor.GPSConfig();
     while(1) 
     {
-        if(sensor.GPSGetData(GPSdata)) 
-			{
-				printf("I'm at %f, %f\n", GPSdata[0],GPSdata[1] );
-			} 
-		else 
-			{
-				printf("Oh Dear! No lock :(\n");
-			}
+		sensor.GPSGetData(GPSData);
+        printf("I'm at %f, %f\n", GPSData[0],GPSData[1] );
+
     }
 }
