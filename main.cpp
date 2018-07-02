707c4c389012fb1329c3c9280dcef0d5f5030b3c
Tsuyoshi Nakashima <nakashima@lss.mes.titech.ac.jp>
2018-07-02 09:11:51 +0900

    modify sensors bug

--- main.cpp
+++ main.cpp
@@ -5,44 +5,41 @@
 #include "Sensor.h"
 #include <stdio.h>
 
+using namespace std;
 
 Sensor sensor;
 
-using namespace std;
-
-double Sdata[9];
+double Mg[3],ACGY[6];
+int s1,s2,m1,m2,i;
+float GPSdata[2];
 
 FILE *fp;
 
 void data();
-int LoopNum=0;
+
 int main()
 {
-	int s1,s2,m1,m2;
+	i=0;
 	gpioInitialise();
 	sensor.getPi();
 	sensor.hmcInit();
 	sensor.mpuInit();
-	sensor.GPSConfig();
-	sensor.hmcSetConfigA(0x1c); //output rate and measurement configuration
+	sensor.hmcSetConfigA(0x18); //output rate and measurement configuration
 	sensor.hmcSetConfigB(0x20);//Recommended Sensor Field Range 
-	sensor.hmcSetMode(0x01);//0x01:single measurement mode(160Hz) 0x00:Continuous measurement mode
+	sensor.hmcSetMode(0x00);//0x01:single measurement mode(160Hz) 0x00:Continuous measurement mode
 	sensor.mpuSetConfig();
-	fp = fopen("/home/pi/sensorlog1.csv","w");
+	fp = fopen("/home/pi/Sensor/sensorlog.csv","w");
 	gpioTime(0,&s1,&m1);
 	fprintf(fp,"%d.%03d seconds\r\n",s1,m1/1000);
 	while(1)
 	{
 		gpioSetTimerFunc(0,10,data);
-	    if(LoopNum>=1500)
+	    if(i>=1000)
 	    {
 			break;
 		}
 	}
-	while(1)
-	{
-		gpioSetTimerFunc(1,10,data2);
-	}
+	//sensor.GPSGetData(GPSdata);
 	gpioTime(0,&s2,&m2);
 	fprintf(fp,"%d.%03d seconds\r\n",s2,m2/1000);
 	fclose(fp);
@@ -51,16 +48,13 @@ int main()
 
 void data()
 {
-	sensor.GetMotion9(Sdata);
-	fprintf(fp,"%f;%f;%f;%f;%f;%f,%f,%f,%f\r\n",Sdata[0],Sdata[1],Sdata[2],Sdata[3],Sdata[4],Sdata[5],Sdata[6],Sdata[8],Sdata[7]);
-	LoopNum++;
+	sensor.hmcGetXYZ(Mg);
+	sensor.mpuGetMotion6(ACGY);
+	fprintf(fp,"%lf;%lf;%lf;%lf;%lf;%lf,%lf,%lf,%lf\r\n",Mg[2],-1*Mg[0],Mg[1],ACGY[0],ACGY[1],ACGY[2],ACGY[3],ACGY[4],ACGY[5]);
+	i++;
 }
 
-void data2()
-{
-	sensor.GetMotion9(Sdata);
-	fprintf(fp,"%f;%f;%f;%f;%f;%f,%f,%f,%f\r\n",Sdata[0],Sdata[1],Sdata[2],Sdata[3],Sdata[4],Sdata[5],Sdata[6],Sdata[8],Sdata[7]);
-}
+
 
 //How To Use--------------------------------------------------------------------------------
 //I2C connect check: $ sudo i2cdetect -y 1
