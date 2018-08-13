0d5477647475ad80fd51bcc6ae89701f8da82dba
Tsuyoshi Nakashima <nakashima@lss.mes.titech.ac.jp>
2018-08-13 17:59:52 +0900

    recording program using raspberry pi camera module v2 and openCV

--- camera_test.cpp
+++ camera_test.cpp
@@ -3,6 +3,8 @@
 #include <opencv2/videoio/videoio.hpp>
 #include <iostream>
 #include <pigpio.h>
+#include <stdio.h>
+
 using namespace cv;
 using namespace std;
 
@@ -10,19 +12,23 @@ int main(int argc, char* argv[])
 {
 	gpioInitialise();
 	int s1, m1, s2, m2;
-
-	Mat mat;
-	VideoCapture vcap(0);
 	int width, height, fourcc;
-	double fps;
+	double fps, i;
+	FILE *fp;
+
+	cv::Mat frame, dst;
+	cv::VideoCapture vcap;
+	cv::VideoWriter writer;
+
+	vcap.open(0);
+	fp = fopen("camera_fps_calc.csv", "w");
+
 	width = (int)vcap.get(cv::CAP_PROP_FRAME_WIDTH);   // フレーム横幅を取得
 	height = (int)vcap.get(cv::CAP_PROP_FRAME_HEIGHT);  // フレーム縦幅を取得
 	fps = vcap.get(cv::CAP_PROP_FPS);                        // フレームレートを取得
-	fourcc = cv::VideoWriter::fourcc('H', '2', '6', '4');       // H.264 / .wmv
-	cv::VideoWriter writer;
-	writer.open("test.avi", fourcc, fps, cv::Size(width, height));
-	cv::Mat frame, dst;
+	fourcc = writer.fourcc('X', 'V', 'I', 'D');
 
+	writer.open("test.avi", fourcc, 13, cv::Size(width, height));
 
 	if (!vcap.isOpened())
 	{
@@ -35,25 +41,28 @@ int main(int argc, char* argv[])
 	while (1)
 	{
 		vcap >> frame;
+		/*
 		if (frame.empty() == true)
 		{
-			cout << "frame empty" << endl;
-			break;
+		cout << "frame empty" << endl;
+		break;
 		}
+		*/
 		cv::flip(frame, dst, 1);
 		writer << dst;
-		cv::waitKey(1000/60);
-
+		i++;
+		//cv::waitKey(10);
 		gpioTime(0, &s2, &m2);
-		if (s2-s1 >= 10)
+		if (s2 - s1 >= 10)
 		{
-			cout << "time up" << endl;
+			writer.release();
+			fprintf(fp, "start:%d.%03dseconds\r\n", s1, m1 / 1000);
+			fprintf(fp, "stop:%d.%03dseconds\r\n", s2, m2 / 1000);
+			fprintf(fp, "loopnumber is %f\r\n", i);
 			break;
 		}
 	}
 	return 0;
 }
 
-//header file  -I/usr/local/include/opencv
-//library      -L/usr/local/lib
-//g++ camera_test.cpp -o test -I/usr/local/include -L/usr/local/lib -lopencv_core -lopencv_highgui -lopencv_videoio
+//g++ camera_test.cpp - o test - I / usr / local / include - L / usr / local / lib - lopencv_core - lopencv_highgui - lopencv_videoio - lpigpio - lrt
