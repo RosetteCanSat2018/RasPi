41a43223e4bb35093eae4ffc949071469bd3810c
hiro_astro <libmo.lifeon@gmail.com>
2018-08-24 12:47:56 +0900

    no message

--- Integrated/BBM/MU2/MU2.cpp
+++ Integrated/BBM/MU2/MU2.cpp
@@ -5,11 +5,11 @@
 #include <unistd.h>
 using namespace std;
 
-
+#include <string>
 
 void MU2::setPin()
 {
-	MU2_handle = serOpen("/dev/ttyS0", 19200, 0);
+	//MU2_handle = serOpen("/dev/ttyS0", 19200, 0);
 	//cout << "MU2_handle=" << MU2_handle << endl;
 }
 
@@ -35,11 +35,12 @@ void MU2::MU2Initialise()
 
 void MU2::Send(char data[])
 {
+	MU2_handle = serOpen("/dev/ttyS0", 19200, 0);
 	char i_data[6];
 	i_data[0] = '@';
 	i_data[1] = 'D';
 	i_data[2] = 'T';
-	
+
 	data_len = strlen(data);
 	data_num = data_len;
 	cout << "data_len =" << data_len << endl;
@@ -62,20 +63,40 @@ void MU2::Send(char data[])
 		serWriteByte(MU2_handle,i_data[counter]);
 		cout << "i_data [" << counter << "] = " << i_data[counter] << endl;
 		usleep(1000);
-	}	
+	}
 
 	for (int data_counter = 0; data_counter < data_num; data_counter++) {
 		serWriteByte(MU2_handle,data[data_counter]);
 		cout << "data [" << data_counter << "] = " << data[data_counter] << endl;
 		usleep(1000);
-	}   
+	}
 	SendTerminal();
 
-	
+
 	//for(int i=0; i<8; i++){
 	//int c = serReadByte(MU2_handle);
 	//cout << "c= " << c << endl;
 	//}
-		
+	serClose(MU2_handle);
+
 }
 
+void MU2::SendGPS(float data){
+	MU2_handle = serOpen("/dev/ttyS0", 19200, 0);
+	string s_GPS;
+	s_GPS = to_string(data);
+	char c_GPS[8] = {};
+	s_GPS.copy(c_GPS, 8);
+	for (int i=0; i<8, i++) {
+		serWriteByte(MU2_handle, c_GPS[i]);
+		usleep(1000);
+	}
+	//serWrite(MU2_handle, c_GPS, 8);
+	SendTerminal();
+	serClose(MU2_handle);
+}
+
+void MU2::closeMU2(){
+	int close = serClose(MU2_handle);
+	cout << close <<endl;
+}
--- Integrated/test/MU2.cpp
+++ Integrated/test/MU2.cpp
@@ -82,12 +82,18 @@ void MU2::Send(char data[])
 }
 
 void MU2::SendGPS(float data){
+	MU2_handle = serOpen("/dev/ttyS0", 19200, 0);
 	string s_GPS;
 	s_GPS = to_string(data);
 	char c_GPS[8] = {};
 	s_GPS.copy(c_GPS, 8);
-	serWrite(MU2_handle, c_GPS, 8);
+	for (int i=0; i<8, i++) {
+		serWriteByte(MU2_handle, c_GPS[i]);
+		usleep(1000);
+	}
+	//serWrite(MU2_handle, c_GPS, 8);
 	SendTerminal();
+	serClose(MU2_handle);
 }
 
 void MU2::closeMU2(){
