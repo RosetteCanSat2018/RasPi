390d9eb99021c7ae6d29a71bbf43331201fe1ceb
Tsuyoshi Nakashima <nakashima@lss.mes.titech.ac.jp>
2018-09-04 22:51:12 +0900

    no message

--- Integrated/BBM/GPS/GPS_test.cpp
+++ Integrated/BBM/GPS/GPS_test.cpp
@@ -5,6 +5,7 @@
 #include "Sensor.h"
 #include <stdio.h>
 #include <math.h>
+#include <signal.h>
 
 using namespace std;
 
@@ -13,34 +14,41 @@ Sensor sensor;
 #define TIMER 3000 //[msec]
 #define COUNTER 10
 
-float GPS[2],GPS_prev[2];
-int GPS_0_counter, GPS_1_counter;
+float GPS[2];// , GPS_prev[2];
+//int GPS_0_counter, GPS_1_counter;
 
 void func();
+void escape(int sig) {
+	cout << "ctrl+c" << endl;
+	exit(1);
+}
 
 int main()
 {
 	gpioInitialise();
-	GPS_prev[0] = 0;
-	GPS_prev[1] = 0;
+	//GPS_prev[0] = 0;
+	//GPS_prev[1] = 0;
 	
 	while(1)
 	{
 		gpioSetTimerFunc(0, TIMER, func);
 		//if (GPS_0_counter > COUNTER && GPS_1_counter > COUNTER && alt_counter > COUNTER)
+		/*
 		if (GPS_0_counter > COUNTER && GPS_1_counter > COUNTER)
 		{
 			cout << "landing" << endl;
 			gpioSetTimerFunc(0, TIMER, NULL);
 			break;
 		}
+		*/
+		signal(SIGINT, escape);
 	}
 }
 
 void func()
 {
 	sensor.GPSGetData_f(GPS);
-	
+	/*
 	if (fabsf(GPS[0] - GPS_prev[0]) <= 0.0002)
 	{
 		GPS_0_counter++;
@@ -65,11 +73,13 @@ void func()
 	{
 		alt_counter = 0;
 	}
-	*/
+	
 	GPS_prev[0] = GPS[0];
 	GPS_prev[1] = GPS[1];
+	*/
 	//alt_prev = alt;
 	//cout << GPS_0_counter << ";" << GPS_1_counter << ";" << alt_counter << endl;
-	cout << GPS[0] << ";" << GPS[1] << ";" << GPS_0_counter << ";" << GPS_1_counter << endl;
+	cout << GPS[0] << ";" << GPS[1] << ";"/* << GPS_0_counter << ";" << GPS_1_counter */<< endl;
+	signal(SIGINT, escape);
 	//cout << alt << ";"<< alt_counter << endl;
 }
---
+++ Integrated/Ballon/Ballon.service.cpp
@@ -0,0 +1,8 @@
+[Unit]
+Description = Ballon
+
+[Service]
+Type = simple
+ExecStart = sudo /home/pi/Ballon/test &
+
+[Install]
